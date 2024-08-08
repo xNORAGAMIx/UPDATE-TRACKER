3b5aa91bf52f49863df70c7ab9a38e883b8116c2
Manas Das <noragami0021@gmail.com>
2024-08-08 11:51:33 +0530

    added route to server

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import usageRoutes from "./routes/usageRoute.js";
+
 import { connectDB } from "../config/dbConnection.js";
 
 dotenv.config();
@@ -13,7 +15,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 5001;
 
-
+app.use("/api/usage", usageRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
